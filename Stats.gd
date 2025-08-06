2d390effa624d0f324d934c3df62c01e110d138b
Lynnkh <lin20021115@yahoo.com.tw>
2025-08-06 11:34:56 +0800

    v2玩家生命UI

--- Stats.gd
+++ Stats.gd
@@ -1,12 +1,20 @@
 extends Node
 
 #最大生命
-@export var max_health:int = 1
+@export var max_health:int = 1 :set = set_max_health
 #目前生命
-@onready var health = max_health:set = set_health
+@onready var health: int = max_health :set = set_health
 
 signal no_health
 signal health_change(value)
+signal max_health_changed(value)
+
+
+func set_max_health(value):
+	max_health = value
+	#生命永遠小魚最大生命
+	self.health = min(health,max_health)
+	emit_signal("max_health_changed",max_health)
 
 func set_health(value):
 	health = value
--- UI/health_ui.gd
+++ UI/health_ui.gd
@@ -5,20 +5,26 @@ var hearts:int = 4:set = set_hearts
 ##最大生命愛心變量
 var max_heart:int = 4:set = set_max_heart
 
-@onready var label = $Label
+#獲取子節點
+@onready var healthUIFull = $HealthUIFull
+@onready var healthUIEmpty = $HealthUIEmpty
 
 func set_hearts(value):
 	hearts = clamp(value, 0, max_heart)
-	#設置label的顯示
-	if label != null:
-		label.text = "HP = " + str(hearts)
+	if healthUIFull != null:
+		healthUIFull.size.x = hearts * 15
 	
 func set_max_heart(value):
 	max_heart = max(value, 1)
-
+	self.hearts = min(hearts, max_heart)
+	if healthUIEmpty != null:
+		healthUIEmpty.size.x = max_heart * 15
+		
 func _ready() -> void:
 	#設置屬性
 	self.max_heart = PlayStats.max_health
 	self.hearts = PlayStats.health
-	#呼叫信號
+	#連接信號
+	#設置愛心UI
 	PlayStats.health_change.connect(self.set_hearts)
+	PlayStats.max_health_changed.connect(self.set_max_heart)
